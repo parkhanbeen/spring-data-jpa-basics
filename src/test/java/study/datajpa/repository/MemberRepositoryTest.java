82a0d3761d49a3f895272fd1170b116f88fe8926
박한빈 <phb0228@naver.com>
2020-12-13 14:15:27 +0900

    반환 타입

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -143,5 +143,22 @@ class MemberRepositoryTest {
             System.out.println("member = " + member);
         }
     }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        System.out.println("findMember = " + findMember);
+
+        Optional<Member> resultOptional = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("resultOptional = " + resultOptional.get());
+
+    }
+
+
 }
 
